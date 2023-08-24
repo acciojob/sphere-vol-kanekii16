9d24a5a8f6fca82208f837c2e69e8edc431117f0
kanekii16 <96989943+kanekii16@users.noreply.github.com>
2023-08-24 11:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function volume_sphere() {
     //Write your code here
+
+	let obj = document.getElementById("radius");
+
+	let r = obj.text;
+
+	let ans = 4 / 3;
+	ans = ans * 3.14;
+	ans = ans * r *r *r;
   
 } 
 
